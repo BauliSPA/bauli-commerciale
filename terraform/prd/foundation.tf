e1888a51d6ce1d63c0010f012070993ac952624e
nico-battelli-sdg <129045603+nico-battelli-sdg@users.noreply.github.com>
2024-10-01 23:49:02 +0200

    fix foundation (#8)

--- terraform/prd/foundation.tf
+++ terraform/prd/foundation.tf
@@ -1,35 +1,26 @@
 # Foundation: Data Admins
-resource "google_project_iam_binding" "admins_binding" {
-  project = local.project_id
-  role    = each.value
-
+resource "google_project_iam_member" "admins_binding" {
   for_each = toset(local.admins_roles)
 
-  members = [
-    "group:data_admins@bauli.it",
-  ]
+  project = local.project_id
+  role    = each.value
+  member  = "group:data_admins@bauli.it"
 }
 
 # Foundation: Data Developers
-resource "google_project_iam_binding" "developers_binding" {
-  project = local.project_id
-  role    = each.value
-
+resource "google_project_iam_member" "developers_binding" {
   for_each = toset(local.developers_roles)
 
-  members = [
-    "group:data_developers@bauli.it",
-  ]
+  project = local.project_id
+  role    = each.value
+  member  = "group:data_developers@bauli.it"
 }
 
 # Foundation: Data Maintainers
-resource "google_project_iam_binding" "maintainers_binding" {
-  project = local.project_id
-  role    = each.value
-
+resource "google_project_iam_member" "maintainers_binding" {
   for_each = toset(local.maintainers_roles)
 
-  members = [
-    "group:data_maintaners@bauli.it", # Typo error in the group creation, not here # TODO: è possibile rinominare il gruppo creato?
-  ]
+  project = local.project_id
+  role    = each.value
+  member  = "group:data_maintaners@bauli.it" # Typo error in the group creation, not here # TODO: è possibile rinominare il gruppo creato?
 }
