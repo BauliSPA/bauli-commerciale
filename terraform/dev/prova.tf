78994402afa170f8ce22bd6531e3efefb4964851
nico-battelli-sdg <nico.battelli@sdggroup.com>
2025-01-10 11:22:04 +0100

    delete bucket test cd ci

--- terraform/dev/prova.tf
+++
@@ -1,6 +0,0 @@
-resource "google_storage_bucket" "test_bucket" {
-  name          = "${local.project_id}-test"
-  location      = local.landing_bucket_location
-  storage_class = local.landing_bucket_storage_class
-  force_destroy = true
-}
