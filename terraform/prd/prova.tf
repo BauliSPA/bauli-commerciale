369692b9d7cb1c2ce77a52073c61bdbb9e7a09dd
nico-battelli-sdg <129045603+nico-battelli-sdg@users.noreply.github.com>
2025-01-10 11:19:52 +0100

    test for cd ci (#13)

---
+++ terraform/prd/prova.tf
@@ -0,0 +1,8 @@
+# To merge for a test of the merge github action and then delete again
+
+resource "google_storage_bucket" "test_bucket" {
+  name          = "${local.project_id}-test"
+  location      = local.landing_bucket_location
+  storage_class = local.landing_bucket_storage_class
+  force_destroy = true
+}
