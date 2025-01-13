c9004e9612ca8ae77eba66b88fb6a96f7a6e7fdf
nico-battelli-sdg <nico.battelli@sdggroup.com>
2025-01-13 14:44:13 +0100

    role and secret for dataform

--- terraform/prd/secrets.tf
+++ terraform/prd/secrets.tf
@@ -56,3 +56,23 @@ resource "google_secret_manager_secret_version" "sftp_password_version" {
 
   depends_on = [google_secret_manager_secret.sftp_password]
 }
+
+# SSH private key secret for GitHub-Dataform link
+data "google_kms_secret" "ssh_private_key_secret_decrypted" {
+  crypto_key = "projects/${local.project_id}/locations/${local.secrets.location}/keyRings/${local.secrets.keyring_name}/cryptoKeys/${local.secrets.key_name}"
+  ciphertext = "CiQAa+yO9IljieCDyyFIhhnMXkSf0fkjqop8DoV0Kksw4cUcG4IShgMA9OVglRoOSthF5Ewnd8Ir6tYvqEajOL3lgh3Zamdsr6WL9KWp+L1B9yYwIV8NmHOnV465qj46biZmKSRpeMQPI75gQKGFB23lh3ydYZZAx7dzw+Na3/VsqYuo5xO6hC9dDz/5AAN/ezZ/+KP/w0dqhsuMdUmcMrZkGaEsYHaQxfYbscQks7kLVdf9fLWoBsqF9N+Hko8n4LDrsHnol3iUa8wx3JmKTSVP8nMK/Jd313b6+qvQwP5DpwsFyHdoxKDYh7ZPREovMkLYbORznVTEcCEAhfi+k8p15qDqNT494nGJrXsu359NVnexd3MKScBeLlDUFrGOq3869s+JokZp5UK+N0xeG59oG+Wh1WLl1v6bstrS809RTm0OVwvnwNjYgZpUiJfoGwJGCJYdUxV7hXL77Erb/fC+YBPMGtNeZeNWXegpLoAnseBcV1hIK49nFg/TFARm42Qt3QAphQbEGPJgw2x6m1uoaMMEhJEJKWpyRqb9GKncvE631ZHUXLODuFf3t7A="
+}
+
+resource "google_secret_manager_secret" "ssh_private_key_secret" {
+  secret_id = "ssh-private-key-secret"
+  replication {
+    auto {}
+  }
+}
+
+resource "google_secret_manager_secret_version" "ssh_private_key_secret_version" {
+  secret      = google_secret_manager_secret.ssh_private_key_secret.id
+  secret_data = data.google_kms_secret.ssh_private_key_secret_decrypted.plaintext
+
+  depends_on = [google_secret_manager_secret.ssh_private_key_secret]
+}
